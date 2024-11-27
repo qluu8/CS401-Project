5f3477d78a4ac4e10a8c444ec53eab648fa81e66
Moeikrey <asotiras@gmail.com>
2024-11-27 09:45:04 -0800

    Added BookManager class with constructors and search, add/remove book

---
+++ BookManager.java
@@ -0,0 +1,36 @@
+/*
+ * Author: Andreas Sotiras 11/27/24
+ * I created a basic template with a bookmanager class. Please look over this @Christian
+ * And add based off what seems to be needed.
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookManager {
+    private List<Book> books;
+
+    public BookManager() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void removeBook(String ISBN) {
+        books.removeIf(book -> book.getISBN().equals(ISBN));
+    }
+
+    public Book searchBook(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null; // Book not found
+    }
+
+    public List<Book> getAllBooks() {
+        return books;
+    }
+}
