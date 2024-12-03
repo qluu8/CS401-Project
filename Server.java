d8d18e6e34e2061fd4c68d81483fec110717ddf7
camoranto1 <157647596+camoranto1@users.noreply.github.com>
2024-12-02 16:12:24 -0800

    Update Server.java
    Added main method and function to save book changes to the catalog when the server closes

--- Server.java
+++ Server.java
@@ -2,14 +2,20 @@
  * Author: Andreas Sotiras
  * SERVER CLASS... Will work once other pieces are implemented.
  */
-import java.io.*;
-import java.net.*;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     private BookManager bookManager;
+    private LoanManager loanManager;
+    private registration registration;
 
-    public Server(BookManager bookManager) {
+    public Server(BookManager bookManager, LoanManager loanManager, registration registration) {
         this.bookManager = bookManager;
+        this.loanManager = loanManager;
+        this.registration = registration;
     }
 
     public void start(int port) {
@@ -17,10 +23,23 @@ public class Server {
             System.out.println("Server started on port: " + port);
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                new ClientHandler(clientSocket, bookManager).start();
+                System.out.println("Client connected from " + clientSocket.getInetAddress());
+                new ClientHandler(clientSocket, bookManager, loanManager, registration).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            // Save books to file on server shutdown
+            bookManager.saveBooksToFile();
         }
+    }
+
+    public static void main(String[] args) {
+        BookManager bookManager = new BookManager();
+        LoanManager loanManager = new LoanManager();
+        registration registration = new registration();
+
+        Server server = new Server(bookManager, loanManager, registration);
+        server.start(8080); // Start the server on port 8080
     }
 }
